08fc53d4ffd6b35f47e71801f40476514d490e9d
ra <40503841+RadiantAeon@users.noreply.github.com>
2025-04-07 21:21:43 -0400

    Update jup_api.rs

--- src/apis/jup_api.rs
+++ src/apis/jup_api.rs
@@ -97,7 +97,7 @@ pub async fn prices_get(
 
     let local_var_client = &local_var_configuration.client;
 
-    let local_var_uri_str = configuration.base_path;
+    let local_var_uri_str = &configuration.base_path;
     let mut local_var_req_builder =
         local_var_client.request(reqwest::Method::GET, local_var_uri_str);
 
